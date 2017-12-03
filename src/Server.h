f6d3821a95a74ed1c784370e01efc4e0284bd763
Chaviva Moshavi <chaviva.moshavi@gmail.com>
2017-12-03 18:26:14 +0200

    sorting main file

---
+++ src/Server.h
@@ -0,0 +1,23 @@
+/*
+ * Server.h
+ *
+ *  Created on: Dec 3, 2017
+ *      Author: djoffe
+ */
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+class Server {
+public:
+	Server(int port);
+	void start();
+	void stop();
+private:
+	int port;
+	int serverSocket; // the socket's file descriptor
+	void handleClient(int clientSocket);
+	int calc(int arg1, const char op, int arg2) const;
+};
+
+#endif /* SERVER_H_ */
