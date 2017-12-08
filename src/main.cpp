3634c248004cd7d88feaad0b236ed3590efd297f
Chaviva Moshavi <chaviva.moshavi@gmail.com>
2017-12-08 11:52:43 +0200

    main will now read port from file, and I  added said file to folder

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,26 @@
 #include "Server.h"
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 using namespace std;
 
 
 int main() {
-	Server server(8000);//read from file
+  int port_num;
+  ifstream port_file;
+  port_file.open("port_number.txt");
+  if(!port_file.is_open()) {
+    cout << "Cannot open file with port number." << endl;
+    return -1;
+  }
+  port_file >> port_num;
+  port_file.close();
+	Server server(port_num);
 	try {
 	  server.start();
 	} catch (const char *msg) {
 		cout << "Cannot start server. Reason: " << msg << endl;
 		exit(-1);
 	}
+
 }
