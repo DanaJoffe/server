910c9991d17cc76aaa57620996bb2ba76b711a15
Dana Joffe <djoffe94@gmail.com>
2017-12-09 12:42:56 -0800

    communication protocol description

--- include/Server.h
+++ include/Server.h
@@ -8,9 +8,24 @@
 #ifndef SERVER_H_
 #define SERVER_H_
 
+enum Status {NO_MOVES, HAS_MOVE, END};
 
-enum Input {NO_MOVES, HAS_MOVE, END};
-
+/*
+ * Communication Protocol description:
+ * the server works with 2 clients, and from each client it gets 2 messages maximum:
+ * 1. the client status (integer)
+ * 2. depends on the status - the client's move (2 integers)
+ *
+ * the logic goes this way: (according to Status enum)
+ * - read the first input and send it to the other client.
+ * - if the first input - the status - is: 0 - stop reading.
+ * 	 									   1 - read move
+ * 	 									   2 - close client's sockets.
+ * - in case of reading a move: server will read 2 integers, and will send them (and a
+ *   separating comma) to the other client.
+ *
+ * all clients know and obey the protocol.
+ */
 class Server {
 public:
 	Server(int port);
@@ -18,10 +33,7 @@ public:
 	void stop();
 	int serverSocket; // the socket's file descriptor
 	void handleTwoClients(int clientSocket, int clientSocket2);
-
-	//CHANGE
 	bool handleOneClient(int clientSocket, int waitingClient);
-
 	bool is_client_closed(int cs);
 
 
