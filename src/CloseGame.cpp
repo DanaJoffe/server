f78e2ec3f9fb4b99492812af3bf63d91ff47a4dc
Chaviva Moshavi <chaviva.moshavi@gmail.com>
2017-12-23 23:09:34 +0200

    took away option of closing game with only 1 player

--- src/CloseGame.cpp
+++ src/CloseGame.cpp
@@ -11,13 +11,6 @@ void CloseGame::execute(vector<string>& args, map<string, vector<int> >& games,
     int client_socket) {
   map<string, vector<int> >::iterator it;
   for (it = games.begin(); it != games.end(); it++) {
-    if (it->second.size() == 1) {
-      if (it->second[0] == client_socket) {
-        close(it->second[0]);
-        games.erase(it);
-        break;
-      }
-    }
     if (it->second.size() == 2) {
       if (it->second[0] == client_socket || it->second[1] == client_socket) {
         close(it->second[0]);
